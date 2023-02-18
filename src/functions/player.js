b7261fd5d1774a6f8a94715eb8d9d2589e6a7f34
EnterOrion <orionwesson@gmail.com>
2023-02-18 22:39:23 +0100

    Player factory function

---
+++ src/functions/player.js
@@ -0,0 +1,40 @@
+import gameboardFactory from "./gameboard";
+
+const player = (name) => {
+    const board = gameboardFactory();
+    let moveArray = [];
+
+    //The values will be obtained by the user clicking on the board
+    const simpleAttack = () => {
+        let x = null;
+        let y = null;
+        return [x, y]
+    }
+    const aiAttack = () => {
+        let x = Math.random() * 9;
+        let y = Math.random() * 9;
+        for (let i=0; i<moveArray.length; i++) {
+            if(parseInt(moveArray[i]) == parseInt([x,y])) {
+                aiAttack();
+            }
+            else {
+                moveArray.push([x,y])
+                return [x, y]
+            }
+        }
+     
+    }
+
+    return {
+        name,
+        board,
+        simpleAttack,
+        aiAttack
+
+    }
+} 
+
+
+
+
+export default player;
